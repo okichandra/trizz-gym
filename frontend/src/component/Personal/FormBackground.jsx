ff3e4155f85ed4bbf764aea23f7f719ba5602902
Oki Chandra Cristianta <setiadgnml@gmail.com>
2026-05-08 13:14:07 +0700

    verry fina

---
+++ frontend/src/component/Personal/FormBackground.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Grainient from '../ReactBitz/Grainient';
+
+function FormBackground() {
+    return (
+        <div id='background' className='col-span-7 relative overflow-hidden'>
+            <Grainient
+                color1="#6911A3"
+                color3="#7D257E"
+                color2="#080F27"
+                timeSpeed={0.25}
+                colorBalance={0}
+                warpStrength={1}
+                warpFrequency={5}
+                warpSpeed={2}
+                warpAmplitude={50}
+                blendAngle={0}
+                blendSoftness={0.05}
+                rotationAmount={500}
+                noiseScale={2}
+                grainAmount={0.1}
+                grainScale={2}
+                grainAnimated={false}
+                contrast={1.5}
+                gamma={1}
+                saturation={1}
+                centerX={0}
+                centerY={0}
+                zoom={0.9}
+
+                className='absolute'
+            />
+            <div className='relative border-red-500 z-10 h-full w-full flex flex-col capitalize justify-end p-10 gap-4'>
+                <h3 className='text-medium text-3xl font-medium w-1/4'>Push <span className='text-light font-semibold'>your limit</span> with us</h3>
+                <span className='text-light w-1/2 text-sm'>train smarter, get stronger, and push your limit with expert coaching.</span>
+            </div>
+        </div>
+    )
+}
+
+export default FormBackground
--- frontend/src/page/Login.jsx
+++ frontend/src/page/Login.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import Grainient from '../component/ReactBitz/Grainient';
 import Logo from '../assets/trizz-logo.svg'
 import Input from '../component/Personal/Input';
 import SubmitButton from '../component/Personal/SubmitButton';
 import { Link } from 'react-router-dom';
+import FormBackground from '../component/Personal/FormBackground';
 
 export default class Login extends Component {
 
@@ -12,38 +12,7 @@ export default class Login extends Component {
             <div className='bg-main-background h-screen w-screen flex items-center justify-center font-rethink-sans'>
                 <div id='login-container'
                     className='w-5/6 h-9/10 bg-secondary-background rounded-lg shadow-[0_0_0_1px_rgba(225,225,225,.1)] grid grid-cols-12'>
-                    <div id='background' className='col-span-7 relative overflow-hidden'>
-                        <Grainient
-                            color1="#6911A3"
-                            color3="#7D257E"
-                            color2="#080F27"
-                            timeSpeed={0.25}
-                            colorBalance={0}
-                            warpStrength={1}
-                            warpFrequency={5}
-                            warpSpeed={2}
-                            warpAmplitude={50}
-                            blendAngle={0}
-                            blendSoftness={0.05}
-                            rotationAmount={500}
-                            noiseScale={2}
-                            grainAmount={0.1}
-                            grainScale={2}
-                            grainAnimated={false}
-                            contrast={1.5}
-                            gamma={1}
-                            saturation={1}
-                            centerX={0}
-                            centerY={0}
-                            zoom={0.9}
-
-                            className='absolute'
-                        />
-                        <div className='relative border-red-500 z-10 h-full w-full flex flex-col capitalize justify-end p-10 gap-4'>
-                            <h3 className='text-medium text-3xl font-medium w-1/4'>Push <span className='text-light font-semibold'>your limit</span> with us</h3>
-                            <span className='text-light w-1/2 text-sm'>train smarter, get stronger, and push your limit with expert coaching.</span>
-                        </div>
-                    </div>
+                    <FormBackground />
                     <div id='login-form' className='col-span-5 p-16 px-28'>
                         <div className='Logo flex flex-row justify-center items-center pb-24 pt-4 gap-2 font-semibold'>
                             <img src={Logo} alt="trizz logo" className='w-12' />
