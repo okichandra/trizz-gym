e80d3881f441976a3616a75a1dd08847a96137da
Oki Chandra Cristianta <setiadgnml@gmail.com>
2026-06-04 19:25:38 +0700

    sidebar svg mini fix

--- frontend/src/component/Personal/SideBar.jsx
+++ frontend/src/component/Personal/SideBar.jsx
@@ -26,7 +26,7 @@ function SideBar() {
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path
                                 stroke="currentColor"
-                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0M12 14a7 7 0 0 0-7 7h14a7 7 0 0 0-7-7" />
+                                strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0M12 14a7 7 0 0 0-7 7h14a7 7 0 0 0-7-7" />
                         </svg>
                         <span className='text-xs'>Account</span>
                     </div>
